208cd35107288781239aeb56d50a1a8ab8fc4c00
Dipaditya Das <DipadityaDas@gmail.com>
2022-01-03 23:47:18 +0530

    Comment file

---
+++ 03-Comment.c
@@ -0,0 +1,10 @@
+/* 
+* This is a Multi-line Comments.
+* There are only two style of comments.
+*/
+
+#include <stdio.h>
+int main() {
+	printf("Hello beautiful people."); // This is a single-line comment
+	return 0;
+}
