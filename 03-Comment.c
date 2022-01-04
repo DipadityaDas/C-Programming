aa34173b3f9b86aae8c8cf13c307caf29ba761f1
dipadityadas <DipadityaDas@gmail.com>
2022-01-04 23:18:44 +0530

    Changes to the comment file

--- 03-Comment.c
+++ 03-Comment.c
@@ -1,10 +1,11 @@
 /* 
-* This is a Multi-line Comments.
-* There are only two style of comments.
+* Author: Dipaditya Das
+* Purpose: This program is printing out my name in the screen.
+* This is a Multi-Line Comment.
 */
 
 #include <stdio.h>
 int main() {
-	printf("Hello beautiful people."); // This is a single-line comment
+	printf("Hi, my name is Dipaditya."); // This is a Single-Line Comment.
 	return 0;
 }
