b24c4f53b171d7dcb43e27cd6ca395d02d552e60
Dipaditya Das <DipadityaDas@gmail.com>
2022-01-10 23:22:05 +0530

    .gitignore is now working

--- 06-InputasCharacter.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	char c = '\0';
-	printf("Enter any character : ");
-	scanf("%c", &c);
-	printf("The Character is %c", c);
-	return 0;
-}
--- 07-Addition.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int a,b,c;
-    scanf("Enter the values of a and b\n%d%d", &a, &b);
-    c = a + b;
-    printf("The sum of %d and %d = %d", a,b,c);
-    return 0;
-}
