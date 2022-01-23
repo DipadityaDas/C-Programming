b5bc7b35635f9db2d3096d97dddb4227e987ef7f
Dipaditya Das <DipadityaDas@gmail.com>
2022-01-23 23:14:57 +0530

    Created For loop

---
+++ 23-forloop.c
@@ -0,0 +1,17 @@
+/********************************************************
+ * Author: Dipaditya Das
+ *
+ * Write a program which prints first 10 Natual numbers.
+ ********************************************************/
+
+#include <stdio.h>
+int main()
+{
+	int i;
+
+	for (i = 1; i < 11; ++i)
+	{
+		printf("%d ", i);
+	}
+	return 0;
+}
