48fa03d1347ae134d2dccdfdba047e198d38d131
Dipaditya Das <DipadityaDas@gmail.com>
2022-01-24 23:14:57 +0530

    Created While Loop

---
+++ 24-Whileloop.c
@@ -0,0 +1,19 @@
+/********************************************************
+ * Author: Dipaditya Das
+ *
+ * Write a program which prints first 10 Natual numbers.
+ ********************************************************/
+
+#include <stdio.h>
+int main()
+{
+	int i = 1;
+
+	while (i <= 10)
+	{
+		printf("%d\n", i);
+		++i;
+	}
+
+	return 0;
+}
